c82c27e199c8a7a18fdd9f8456e26473e514f8f6
George Paley <George@JDPaley.com>
2017-06-15 12:02:53 -0400

    join script work

--- sql/cv_dev_queries.sql
+++ sql/cv_dev_queries.sql
@@ -3,3 +3,9 @@ select * from edu;
 select * from wrk;
 select * from dty;
 select * from skl;
+SELECT a.ord_num,b.cust_name,a.cust_code,
+c.agent_code,b.cust_city
+FROM agents c,customer b,orders a
+WHERE b.cust_city=c.working_area
+AND a.cust_code=b.cust_code
+AND a.agent_code=c.agent_code;
