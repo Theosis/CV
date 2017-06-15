bdab054f17c0e8e65ea62027903a40a47a659506
George Paley <George@JDPaley.com>
2017-06-15 17:38:24 -0400

    assembling print_cv.sql from 4 section queries

--- sql/print_cv.sql
+++ sql/print_cv.sql
@@ -1,32 +1,23 @@
-SELECT * FROM cv.person;
-select * from cv.edu;
-select * from cv.wrk;
-select * from cv.dty;
-select * from cv.skl;
-
-select concat('\n\n- ', (select group_concat(d.duty separator '\n- ') from dty d)) as duty_list;
-
 use cv;
+
 select concat('\n=============================================================  \n'
 , p.name, '\n', p.email, '\n\n') as pers_info 
 from person p 
 where id = 1; -- PERSONAL INFO
+ 
 
 select concat('Education  \n', 
 (select group_concat(e.ach, ',  \n', e.org, ', ', e.yr, '  \n\n' separator '') 
 from edu e 
 where persId = 1)) as edu_info ; -- EDUCATION INFO
 
-select role, org, era, duty 
-from wrk w left outer join dty d 
-on w.id = d.wrkId 
-where persId = 1; -- GOOD WORK + DUTIES QUERY (won't use - just for reference)
 
 select distinct w.role, w.org, w.era, 
 (select concat('\n\n- ', (select group_concat(d.duty separator '\n- ') from dty d where d.wrkId = w.id))) as duty_list 
 from wrk w left outer join dty d 
 on w.id = d.wrkId 
 where persId = 1; -- BEST WORK + DUTIES QUERY!
+
 
 select concat('Skills\n', 
 (select group_concat(s.skl, ', ', s.lvl, '\n' separator '') 
