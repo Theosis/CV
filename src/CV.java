685f558e5fb6b49362e9a47e6da0401eda6d4e33
George Paley <George@JDPaley.com>
2017-06-16 18:04:56 -0400

    ...

--- src/CV.java
+++ src/CV.java
@@ -25,14 +25,9 @@ public class CV extends HttpServlet {
 	
 	ConnectionManager cm = new ConnectionManager();
 	Statement stm;
-	ResultSet rs;
-	
-//	//DB handles
-//	Connection con = null;
-//	Statement stmt = null;
-//	String sql, psql;
-//	PreparedStatement pst = null;
-//	ResultSet rs = null;
+	ResultSet rs = null;
+	String psql;
+	PreparedStatement pst = null;
 		
     public CV() {
         super();        
@@ -44,112 +39,65 @@ public class CV extends HttpServlet {
 		
 		
 		//fVariables are for form-filling:
-		String fName, fEmail, fEdAch, fEdAchOrg, fEdAchYr; 
-		String fWrkXp, fWrkXpOrg, fWrkXpYrs; 
-		ArrayList<String> duties = new ArrayList<String>();
-		String fSkill, fSkillLvl;
-		boolean done;
-		int numItems;
-		Person fPerson;
-		
-		fPerson = new Person();
+		String fName, fEmail; 
+		String fEdAch, fEdAchOrg, fEdAchYr; 
+//		String fWrkXp, fWrkXpOrg, fWrkXpYrs; 
+//		ArrayList<String> duties = new ArrayList<String>();
+//		String fSkill, fSkillLvl;
+//		boolean done;
+//		int numItems;
+//		Person fPerson;
+//		
+//		fPerson = new Person();
 		
 		String nextURL = "/view.jsp";
 		
 		fName = request.getParameter("fName");
-		fEmail = request.getParameter("fEmail");
-		
-		//println("\n**************\n"+fName+"\n**************\n");
-		
+		fEmail = request.getParameter("fEmail");		
 		
 		Connection con = cm.getConnection();
-		String query = "";
-		String cv = "";
+		String cv = "";	
 		
-		query = "SELECT name, email FROM person WHERE name like '%"+ fName + "%'";
 		try{		
-			stm = con.createStatement();
-			rs = stm.executeQuery(query);
-			String personData = "";
+			psql = "select concat('\n<br>=============================================================  \n<br>', p.name, '\n<br>', p.email, '\n<br>\n<br>') as pers_info from person p where id = 1";
+			pst = con.prepareStatement(psql);
+			fName = "%"+fName+"%" ;
+//			println(fName);
+//			pst.setString(1, fName);
+//			println(psql);
+//			println(pst.toString());
+			rs = pst.executeQuery();
 		
 			while(rs.next()){
-				
-				String personRow = rs.getString(1) + "<br>"	+ rs.getString(2);
-				personData = personData.concat(personRow);
-				request.setAttribute("msg", personData);
+			cv = cv.concat(rs.getString(1));
 			}
+			
+			psql = "select concat('Education  \n<br>', (select group_concat(e.ach, ',  \n<br>', e.org, ', ', e.yr, '  \n<br>\n<br>' separator '') from edu e where persId = 1)) as edu_info";
+			pst = con.prepareStatement(psql);
+			rs = pst.executeQuery();
+			while(rs.next()){
+			cv = cv.concat(rs.getString(1));
+			}
+			
+			psql = "select distinct w.role, w.org, w.era, (select concat('\n<br>\n<br>- ', (select group_concat(d.duty separator '\n<br>- ') from dty d where d.wrkId = w.id))) as duty_list from wrk w left outer join dty d on w.id = d.wrkId where persId = 1";
+			pst = con.prepareStatement(psql);
+			rs = pst.executeQuery();
+			cv = cv.concat("Experience  \\n<br>");
+			while(rs.next()){
+			cv = cv.concat(rs.getString(1)+"\n<br>"+rs.getString(2)+", "+rs.getString(3)+"\n<br>\n<br>"+rs.getString(4));
+			}
+			
+			psql = "select concat('Skills\n<br>', (select group_concat(s.skl, ', ', s.lvl, '\n<br>' separator '') from skl s where s.persId = 1) ) as skl_list";
+			pst = con.prepareStatement(psql);
+			rs = pst.executeQuery();
+			while(rs.next()){
+			cv = cv.concat(rs.getString(1));
+			}
+			
+//			println(cv);
+			request.setAttribute("msg", cv);
 			getServletContext().getRequestDispatcher(nextURL).forward(request,response);
-		   }catch(Exception e){e.printStackTrace();}
-		
-////		WEB CV PERSONAL INFO
-//		
-//		query = "select concat('\n=============================================================  \n', p.name, '\n', p.email, '\n\n') as pers_info from person p where id = 1;" ;
-//		try{		
-//			stm = con.createStatement();
-//			rs = stm.executeQuery(query);
-//					
-////			while(rs.next()){
-//				
-//				cv = cv.concat(request.getParameter("pers_info"));
-////				String personRow = rs.getString(1) + "<br>"	+ rs.getString(2);
-////				personData = personData.concat(personRow);
-//				println(cv);
-//				request.setAttribute("msg", cv);
-////			}
-//			getServletContext().getRequestDispatcher(nextURL).forward(request,response);
-//		   }catch(Exception e){e.printStackTrace();}
-		
-//		WEB CV EDUCATION
-		
-//		query = "select concat('Education  \n', (select group_concat(e.ach, ',  \n', e.org, ', ', e.yr, '  \n\n' separator '') from edu e where persId = 1)) as edu_info;" ;
-//		try{		
-//			stm = con.createStatement();
-//			rs = stm.executeQuery(query);
-//			String personData = "";
-//		
-//			while(rs.next()){
-//				
-//				String personRow = rs.getString(1) + "<br>"	+ rs.getString(2);
-//				personData = personData.concat(personRow);
-//				request.setAttribute("msg", personData);
-//			}
-//			getServletContext().getRequestDispatcher(nextURL).forward(request,response);
-//		   }catch(Exception e){e.printStackTrace();}
-//		
-////		WEB CV EXPERIENCE
-//		
-//		query = "select distinct w.role, w.org, w.era, (select concat('\n\n- ', (select group_concat(d.duty separator '\n- ') from dty d where d.wrkId = w.id))) as duty_list from wrk w left outer join dty d on w.id = d.wrkId where persId = 1;" ;
-//		try{		
-//			stm = con.createStatement();
-//			rs = stm.executeQuery(query);
-//			String personData = "";
-//		
-//			while(rs.next()){
-//				
-//				String personRow = rs.getString(1) + "<br>"	+ rs.getString(2);
-//				personData = personData.concat(personRow);
-//				request.setAttribute("msg", personData);
-//			}
-//			getServletContext().getRequestDispatcher(nextURL).forward(request,response);
-//		   }catch(Exception e){e.printStackTrace();}
-//		
-////		WEB CV SKILLS
-//		
-//		query = "select concat('Skills\n', (select group_concat(s.skl, ', ', s.lvl, '\n' separator '') from skl s where s.persId = 1) ) as skl_list;" ;
-//		try{		
-//			stm = con.createStatement();
-//			rs = stm.executeQuery(query);
-//			String personData = "";
-//		
-//			while(rs.next()){
-//				
-//				String personRow = rs.getString(1) + "<br>"	+ rs.getString(2);
-//				personData = personData.concat(personRow);
-//				request.setAttribute("msg", personData);
-//			}
-//			getServletContext().getRequestDispatcher(nextURL).forward(request,response);
-//		   }catch(Exception e){e.printStackTrace();}
-		
+		   }catch(Exception e){e.printStackTrace();}		
 		
 	}
 	//****************************************************************************************
